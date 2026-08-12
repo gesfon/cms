c228892c5a18d7a92554b0ff3193eeee8c2c0f3e
Goncalo Fontoura <gesfappstore@gmail.com>
2026-08-12 18:28:20 +0200

    småfix

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useActionState } from 'react';
 import { getArticles, addPostAction, deleteById } from '@/app/actions/posts';
-import Link from 'next/link';
 import { logoutAction } from '@/app/actions/auth';
 
 export default function Dashboard() {
@@ -27,59 +26,69 @@ export default function Dashboard() {
   const [state, formAction, isPending] = useActionState(addPostAction, null);
 
     return (
-        <section className="my-40 flex items-center justify-center h-screen h-full text-gray-400 body-font">
+        <section className="flex items-center justify-center h-full text-gray-400 body-font">
             <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
                 <div className="lg bg-gray-800 bg-opacity-50 rounded-lg p-8 flex flex-col md:mx-auto w-full mt-10 md:mt-0">
                     <form action={formAction}>
                         {state?.error && <p style={{ color: 'red' }}>{state.error}</p>}
-                    <h2 className="text-white text-lg font-medium title-font mb-5">Add Article (<Link href="/login" onClick={handleLogout} className="text-indigo-400 hover:text-indigo-300">Logout</Link>)</h2>
-                    <div className="relative mb-4">
-                        <label htmlFor="title" className="leading-7 text-sm text-gray-400">Title</label>
-                        <input type="text" id="title" name="title" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        <h2 className="text-white text-lg font-medium title-font mb-5">Create Article (<a href="#" onClick={handleLogout} className="text-indigo-400 hover:text-indigo-300">Logout</a>)</h2>
+                        <div className="relative mb-4">
+                            <label htmlFor="title" className="leading-7 text-sm text-gray-400">Title</label>
+                            <input type="text" id="title" name="title" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label htmlFor="excerpt" className="leading-7 text-sm text-gray-400">Excerpt</label>
+                            <input type="text" id="excerpt" name="excerpt" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label htmlFor="content" className="leading-7 text-sm text-gray-400">Content</label>
+                            <textarea rows="10" id="content" name="content" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>
+                        <div className="relative mb-4">
+                            <label htmlFor="image" className="leading-7 text-sm text-gray-400">Image</label>
+                            <input type="text" id="image" name="image" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div> 
+                        <div className="relative mb-4">
+                            <label htmlFor="category" className="leading-7 text-sm text-gray-400">Category</label>
+                            <input type="text" id="category" name="category" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                        </div>                                        
+                        <button type="submit" disabled={isPending} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                            {isPending ? 'Creating article...' : 'Add Article'}
+                        </button>
+                        <hr className="my-5" />                    
+                        <div className="overflow-x-auto rounded-lg border border-gray-700">
+                        <table className="min-w-full divide-y-2 divide-gray-700 bg-slate-900 text-sm">
+                            {/*
+                            <thead className="bg-slate-900 text-left font-medium text-gray-100">
+                            <tr>
+                                <th className="px-4 py-3">&#160;</th>
+                                <th className="px-4 py-3">Title</th>
+                                <th className="px-4 py-3">&#160;</th>
+                            </tr>
+                            </thead> 
+                            */}
+                            <tbody className="divide-y divide-gray-700 text-gray-100">
+                                {articles && articles.map((a, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td className="px-4 py-3 font-medium text-gray-100">
+                                                <img className="object-cover object-center" src={a.image} alt={a.title} width={100} height={100} />
+                                            </td>
+                                            <td className="px-4 py-3 font-medium text-gray-100">{a.title}</td>
+                                            <td className="px-4 py-3">
+                                                <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-sm" onClick={() => deleteRow(a.id)}>
+                                                    Remove
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="excerpt" className="leading-7 text-sm text-gray-400">Excerpt</label>
-                        <input type="text" id="excerpt" name="excerpt" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="content" className="leading-7 text-sm text-gray-400">Content</label>
-                        <textarea rows="10" id="content" name="content" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="image" className="leading-7 text-sm text-gray-400">Image</label>
-                        <input type="text" id="image" name="image" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div> 
-                    <div className="relative mb-4">
-                        <label htmlFor="category" className="leading-7 text-sm text-gray-400">Category</label>
-                        <input type="text" id="category" name="category" className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-indigo-900 rounded border border-gray-600 focus:border-indigo-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>                                        
-                    <button type="submit" disabled={isPending} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                        {isPending ? 'Creating article...' : 'Add Article'}
-                    </button>
-                    <hr className="my-5" />                    
-                    <div className="overflow-x-auto rounded-lg border border-gray-200">
-                    <table className="min-w-full divide-y-2 divide-gray-200 bg-slate-700 text-sm">
-                        <thead className="bg-slate-700 text-left font-medium text-gray-100">
-                        <tr>
-                            <th className="px-4 py-3">Title</th>
-                            <th className="px-4 py-3">&#160;</th>
-                        </tr>
-                        </thead>
-                        <tbody className="divide-y divide-gray-200 text-gray-100">
-                            {articles && articles.map((a, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td className="px-4 py-3 font-medium text-gray-100">{a.title}</td>
-                                        <td className="px-4 py-3"><button className="text-red-500 hover:text-red-700" onClick={() => deleteRow(a.id)}>Remove</button></td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                    </div>
-                    </form>
-                </div>
+                </form>
             </div>
-        </section>
+        </div>
+    </section>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 'use client';
 
-import Image from "next/image";
+// import Image from "next/image";
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 export default function Home() {
 
@@ -26,16 +27,17 @@ export default function Home() {
   };
 
   return (
+    <>
     <section className="h-full h-screen text-gray-400 body-font bg-gray-900">
       <div className="flex flex-wrap -m-4">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap w-full mb-20">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Articles list</h1>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Listing Available Articles</h1>
             <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           </div>
           <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">
-            This is a demo page with dummy data. You should truncate the dev.db sqlite3 file to add new data. If adding new tables to the database or doing ALTER TABLE run "npx prisma generate" to match the schema and for new data objects to be available. There's also a posts API available at /api/posts.
+            This is a demo page with dummy data and you should delete / replace it. Truncate the dev.db sqlite3 file if adding new data. If adding new tables to the database or doing ALTER TABLEs you should change the schema.prisma file and run "npx prisma generate" to match the schema and for new data objects to be available. There is also a posts API available at "/api/posts".
           </p>
         </div>        
         <div className="flex flex-wrap -m-4">
@@ -43,7 +45,7 @@ export default function Home() {
               <div key={article.id} className="xl:w-1/4 md:w-1/2 p-4">
                 <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
                   {article.image && (
-                  <Image className="object-cover object-center mb-6" src={article.image} alt={article.title} width={300} height={160} />
+                    <img className="object-cover object-center mb-6" src={article.image} alt={article.title} width={300} height={160} />
                   )}
                   <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">{article.category}</h3>
                   <h2 className="text-lg text-white font-medium title-font mb-4">{truncateText(article.title, 30)}</h2>
@@ -55,6 +57,14 @@ export default function Home() {
         </div>
       </div>  
     </section>
+    <footer className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">© 2026 <a href="https://designr.se" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">Designr.se</a> —
+          <Link href="/login" className="ml-2 text-indigo-400 hover:text-indigo-300">Login here</Link>
+        </p>
+      </div>
+    </footer>
+  </>
   );
 }
 
