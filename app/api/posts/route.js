e0176d5f6b1ffa613589569a56f9adad71324257
Goncalo Fontoura <gesfappstore@gmail.com>
2026-08-12 15:39:31 +0200

    default page.js - articles listing

---
+++ app/api/posts/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { prisma } from "@/lib/prisma";
+
+const posts = await prisma.posts.findMany(
+  {
+    orderBy: {
+      createdAt: 'desc'
+    }
+  }
+);
+
+export async function GET() {
+  return NextResponse.json(posts);
+}
--- app/page.js
+++ app/page.js
@@ -1,69 +1,60 @@
+'use client';
+
 import Image from "next/image";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch("/api/posts");
+      const articlesData = await response.json();
+      setArticles(articlesData);
+    };
+
+    fetchArticles();
+  }, []);
+
+  const truncateText = (text, length) => {
+    if (text.length <= length) {
+      return text;
+    }
+
+    return text.substring(0, length) + '\u2026';
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.js
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <section className="h-full h-screen text-gray-400 body-font bg-gray-900">
+      <div className="flex flex-wrap -m-4">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap w-full mb-20">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Articles</h1>
+            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+          </div>
+          <p className="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">
+            {/* Truncate dev.db sqlite3 file to add new data. */}
           </p>
+        </div>        
+        <div className="flex flex-wrap -m-4">
+          {articles && articles.map((article) => (
+              <div key={article.id} className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+                  {article.image && (
+                  <Image className="object-cover object-center mb-6" src={article.image} alt={article.title} width={300} height={160} />
+                  )}
+                  <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">{article.category}</h3>
+                  <h2 className="text-lg text-white font-medium title-font mb-4">{truncateText(article.title, 30)}</h2>
+                  <p className="leading-relaxed text-base">{truncateText(article.excerpt, 100)}</p>
+                </div>
+              </div>
+          ))}
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>  
+    </section>
   );
 }
+
